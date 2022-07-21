0f6c531cc8255c6300577f9c71d966dde40c282c
Eran Amir <eran.amir@gmail.com>
2022-07-21 22:06:53 +0100

    pretty import but got a NullPointerException on FSM

--- import_policies.py
+++ import_policies.py
@@ -75,20 +75,15 @@ def main_import():
     print('--------------------------------------------------------')
     # print total enabled policies on source fsm
     print('\nStarting to import ' + policy_type + ' policies...')
-    print('--------------------------------------------------------')
-    #print('Total ' + policy_type + ' policies imported: ' + str(total_enabled_policies))
-    #print('--------------------------------------------------------')
-
     print('Target FSM: ' + target_fsm_server)
     # get token from target fsm
     print('\nSending request to get JWT from target FSM')
-    print('--------------------------------------------------------')
-    
-    
+        
     failed_to_import_policies = 0
     notsupported_policies = 0
 
     target_token = get_jwt.get_access_token(target_fsm_server, tgt_user_name, tgt_user_pwd)
+    print('--------------------------------------------------------')
 
     # import polices and rules from source fsm, itterate over the list of policies and refresh token along the way
     for pn in enabled_policies['enabled_policies']:
@@ -103,63 +98,54 @@ def main_import():
 
         if policy_name == 'Email DLP Policy' or policy_name == 'Web DLP Policy':
             notsupported_policies = notsupported_policies + 1
-            print('\nImporting ' + policy_type + ' policy: ' + policy_name)
+            print(policy_name + ': Quick Policies are not supported and will not be imported')
             print('--------------------------------------------------------')
-            print(policy_name + ' : Quick Policies are not supported and will not be imported')
             continue
 
         # Load and import DLP policies and rules from source fsm
         if policy_type == 'DLP':
             print('\nLoading ' + policy_type + ' policy: ' + policy_name)
-            print('--------------------------------------------------------')
             rules_classifiers_output = common.GetPoliciesFromFile.get_rules_classifiers(logger, policy_name)
             severity_action_output = common.GetPoliciesFromFile.get_severity_action(logger, policy_name)
             source_destination_output = common.GetPoliciesFromFile.get_source_destination(logger, policy_name)
             print('\nImporting to FSM:' + policy_type + '  policy: ' + policy_name)
-            print('--------------------------------------------------------')
             return_status = common.PostPolicies.post_rules_classifiers(logger, target_token, target_fsm_server, rules_classifiers_output, policy_name)
             if not (return_status > 200 ): # 200 OK
                 common.PostPolicies.post_severity_action(logger, target_token, target_fsm_server, severity_action_output, policy_name)
                 common.PostPolicies.post_source_destination(logger, target_token, target_fsm_server, source_destination_output,
                                                  policy_name)
             else: 
-                print('\nImport ' + policy_name + ' FAILED.\nHTTP request returned with status code:' + str(return_status))
+                print('\nImport ' + policy_name + ' Failed!.\nHTTP request returned with status code:' + str(return_status))
                 print('See log for details')
-                print('--------------------------------------------------------')
+                failed_to_import_policies += 1
 
         else: # Discovery Policy
             print('\nLoading ' + policy_type + ' policy: ' + policy_name)
-            print('--------------------------------------------------------')
             rules_classifiers_output = common.GetPoliciesFromFile.get_rules_classifiers(logger, policy_name)
             severity_action_output = common.GetPoliciesFromFile.get_severity_action(logger, policy_name)
 
             print('\nImporting to FSM: ' + policy_type + ' policy: ' + policy_name)
-            print('--------------------------------------------------------')
             return_status = common.PostPolicies.post_rules_classifiers(logger, target_token, target_fsm_server, rules_classifiers_output, policy_name)
             if not (return_status > 200 ): # 200 OK
                 common.PostPolicies.post_severity_action(logger, target_token, target_fsm_server, severity_action_output, policy_name)
             else: 
-                print('\nImport ' + policy_name + ' FAILED.\nHTTP request returned with status code:' + str(return_status))
+                print('\nImport ' + policy_name + ' Failed!.\nHTTP request returned with status code:' + str(return_status))
                 print('See log for details')
-                print('--------------------------------------------------------')             
+    print('--------------------------------------------------------')
 
     # import and export rule exceptions if they exist
     if len(rule_exceptions) == 0:
         pass
     else:
         print('\nLoading rule exceptions')
-        print('--------------------------------------------------------')
         for i in rule_exceptions['exception_rules']:
             rule_name = i['rule_name']
             rule_exception_output = common.GetPoliciesFromFile.get_rule_exception(policy_type, rule_name)
 
             print('\nImporting to FSM: ' + policy_type + ' policy: ' + policy_name + " exceptions:" + str(i))
-            print('--------------------------------------------------------')
             common.PostPolicies.post_rule_exception(target_token, target_fsm_server, rule_exception_output, rule_name)
 
-
-
-    # print execution time in seconds
+    print('--------------------------------------------------------')
     print('\nImport task completed:')
     print('Total policies enabled:      ' + str(total_enabled_policies))
     print('Total policies imported:     ' + str(total_enabled_policies - notsupported_policies - failed_to_import_policies))
